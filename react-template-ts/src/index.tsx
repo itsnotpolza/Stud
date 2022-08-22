44c0bb7f6f7f19701406c980e72fa114c2ba1aae
polza <polza.isme@gmail.com>
2022-08-22 21:48:14 +0300

    delete react templates yuck

--- react-template-ts/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <div>App content</div>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
--- react-template/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <div>App here</div>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
